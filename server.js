1cb4840b90a74d9d6d0389126a356978ef4d1e5b
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-10-10 14:42:33 +0200

    use " for strings as opposed \'

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-var express = require('express');
-var http = require('http');
+var express = require("express");
+var http = require("http");
+var routes = require("./routes");
 
 var app = express();
 
 app.configure(function(){
-    app.set('port', process.env.PORT || 3000);
+    app.set("port", process.env.PORT || 3000);
 });
 
-app.configure('development', function(){
+app.configure("development", function(){
     app.use(express.errorHandler());
 });
 
-app.get('/', function(req, res){
-    res.send('Hello World');
-});
+app.get("/", routes.all);
+app.post("/", routes.add);
 
-http.createServer(app).listen(app.get('port'), function(){
-    console.log("Deck Remote Server listening on port " + app.get('port'));
+http.createServer(app).listen(app.get("port"), function(){
+    console.log("Deck Remote Server listening on port " + app.get("port"));
 });
